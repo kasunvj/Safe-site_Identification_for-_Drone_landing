66f0c2ab8f465f7c61c39ea18ab369f12214ad2a
martian <vimukthike@gmail.com>
2019-02-06 01:06:59 +0530

    CNN python work

---
+++ CNN_dogcat.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+import pandas as pd
+import scipy
+import sklearn
+import keras
+from keras.models import Sequential
+import cv2
+from skimage import io
+#%matplotlib inline
